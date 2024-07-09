93f92a7890c267390e094d5ce87de0b68ee3ddf6
AxelanO7 <axel@btwedutech.com>
2024-07-10 06:40:17 +0800

    refactor: Update Perhitungan controller to optimize employee ranking algorithm and improve performance

--- application/controllers/Perhitungan.php
+++ application/controllers/Perhitungan.php
@@ -23,11 +23,32 @@ class Perhitungan extends CI_Controller
             </script>
 <?php
         }
-
+        $variabels = $this->Perhitungan_model->get_variabel();
+        $alternatifs = $this->Perhitungan_model->get_alternatif();
+        foreach ($alternatifs as $alternatif) {
+            $id_karyawan = $alternatif->id_karyawan;
+            $tot = 0;
+            $hasil = [];
+            foreach ($variabels as $variabel) {
+                $id_kriteria = $variabel->id_kriteria;
+                $himpunans = $this->Perhitungan_model->get_himpunan($id_kriteria);
+                foreach ($himpunans as $himpunan) {
+                    $id_subkriteria_fuzzy = $himpunan->id_subkriteria_fuzzy;
+                    $hasil = $this->Perhitungan_model->get_hasil($id_karyawan, $id_kriteria);
+                    if ($hasil->id_subkriteria_fuzzy == $himpunan->id_subkriteria_fuzzy) {
+                        $tot += $hasil->f;
+                    }
+                }
+            }
+            $alternatif->total = $tot / count($variabels);
+        }
+        usort($alternatifs, function ($a, $b) {
+            return $b->total <=> $a->total;
+        });
         $data = [
             'page' => "Perhitungan",
-            'variabels' => $this->Perhitungan_model->get_variabel(),
-            'alternatifs' => $this->Perhitungan_model->get_alternatif(),
+            'variabels' => $variabels,
+            'alternatifs' => $alternatifs,
         ];
         $this->load->view('perhitungan/index', $data);
     }
