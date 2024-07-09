bcab30b854321a9c86bc30ddf7ae575267adfd68
AxelanO7 <axel@btwedutech.com>
2024-07-10 06:34:21 +0800

    refactor: Update Hasil controller to calculate and display total score for each alternatif

--- application/controllers/Hasil.php
+++ application/controllers/Hasil.php
@@ -1,28 +1,48 @@
 <?php
-    
-    defined('BASEPATH') OR exit('No direct script access allowed');
-    
-    class Hasil extends CI_Controller {
-    
-        public function __construct()
-        {
-            parent::__construct();
-            $this->load->library('pagination');
-            $this->load->library('form_validation');
-            $this->load->model('Perhitungan_model');
-        }
 
-        public function index()
-        {
-            $data = [
-                'page' => "Hasil",
-				'variabels'=> $this->Perhitungan_model->get_variabel(),
-                'alternatifs'=> $this->Perhitungan_model->get_alternatif(),
-            ];
-			
-            $this->load->view('hasil/index', $data);
-        }
-    
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Hasil extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('pagination');
+        $this->load->library('form_validation');
+        $this->load->model('Perhitungan_model');
     }
-    
-    
+
+    public function index()
+    {
+        $variabels = $this->Perhitungan_model->get_variabel();
+        $alternatifs = $this->Perhitungan_model->get_alternatif();
+        foreach ($alternatifs as $alternatif) {
+            $id_karyawan = $alternatif->id_karyawan;
+            $tot = 0;
+            $hasil = [];
+            foreach ($variabels as $variabel) {
+                $id_kriteria = $variabel->id_kriteria;
+                $himpunans = $this->Perhitungan_model->get_himpunan($id_kriteria);
+                foreach ($himpunans as $himpunan) {
+                    $id_subkriteria_fuzzy = $himpunan->id_subkriteria_fuzzy;
+                    $hasil = $this->Perhitungan_model->get_hasil($id_karyawan, $id_kriteria);
+                    if ($hasil->id_subkriteria_fuzzy == $himpunan->id_subkriteria_fuzzy) {
+                        $tot += $hasil->f;
+                    }
+                }
+            }
+            $alternatif->total = $tot / count($variabels);
+        }
+        usort($alternatifs, function ($a, $b) {
+            return $b->total <=> $a->total;
+        });
+        echo "<script>console.log('Debug Objects: " . json_encode($alternatifs) . "' );</script>";
+        $data = [
+            'page' => "Hasil",
+            'variabels' => $variabels,
+            'alternatifs' => $alternatifs,
+        ];
+        $this->load->view('hasil/index', $data);
+    }
+}
